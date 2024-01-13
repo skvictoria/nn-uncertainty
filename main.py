ca98e91c806ac5e29546977f8809d1bd44ec6e9d
skvic <skvictoria.sk@gmail.com>
2024-01-13 18:47:32 -0500

    initial commit for RISE model

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+
+from utils import *
+from explanations import RISE
+
+cudnn.benchmark = True
+
+args = Dummy()
+
+# Number of workers to load data
+args.workers = 8
+# Directory with images split into class folders.
+# Since we don't use ground truth labels for saliency all images can be 
+# moved to one class folder.
+args.datadir = '/scratch2/Datasets/imagenet/ILSVRC2012_val_folders/'
+# Sets the range of images to be explained for dataloader.
+args.range = range(95, 105)
+# Size of imput images.
+args.input_size = (224, 224)
+# Size of batches for GPU. 
+# Use maximum number that the GPU allows.
+args.gpu_batch = 250
+
+dataset = datasets.ImageFolder(args.datadir, preprocess)
+
+# This example only works with batch size 1. For larger batches see RISEBatch in explanations.py.
+data_loader = torch.utils.data.DataLoader(
+    dataset, batch_size=1, shuffle=False,
+    num_workers=args.workers, pin_memory=True, sampler=RangeSampler(args.range))
+
+print('Found {: >5} images belonging to {} classes.'.format(len(dataset), len(dataset.classes)))
+print('      {: >5} images will be explained.'.format(len(data_loader) * data_loader.batch_size))
