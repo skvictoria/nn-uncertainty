98204e35d2a49382850ddfa45f994a133de442f4
skvictoria <skvictoria.sk@gmail.com>
2024-01-14 23:19:22 -0500

    add imagenet for explainability with RISE

--- main.py
+++ main.py
@@ -12,7 +12,54 @@ import torchvision.datasets as datasets
 import torchvision.models as models
 
 from utils import *
-from explanations import RISE
+from RISE import RISE
+
+def example(img, top_k=3, save_path='output.png'):
+    saliency = explainer(img.cuda()).cpu().numpy()
+    p, c = torch.topk(model(img.cuda()), k=top_k)
+    p, c = p[0], c[0]
+    
+    plt.figure(figsize=(10, 5*top_k))
+    for k in range(top_k):
+        plt.subplot(top_k, 2, 2*k+1)
+        plt.axis('off')
+        plt.title('{:.2f}% {}'.format(100*p[k], get_class_name(c[k])))
+        tensor_imshow(img[0])
+
+        plt.subplot(top_k, 2, 2*k+2)
+        plt.axis('off')
+        plt.title(get_class_name(c[k]))
+        tensor_imshow(img[0])
+        
+        plt.imshow(saliency[c[k]], cmap='jet', alpha=0.5)
+        plt.colorbar(fraction=0.046, pad=0.04)
+
+    plt.savefig(save_path, bbox_inches='tight')
+    plt.close()
+
+def explain_all(data_loader, explainer):
+    # Get all predicted labels first
+    target = np.empty(len(data_loader), np.int)
+    for i, (img, _) in enumerate(tqdm(data_loader, total=len(data_loader), desc='Predicting labels')):
+        p, c = torch.max(model(img.cuda()), dim=1)
+        target[i] = c[0]
+
+    # Get saliency maps for all images in val loader
+    explanations = np.empty((len(data_loader), *args.input_size))
+    for i, (img, _) in enumerate(tqdm(data_loader, total=len(data_loader), desc='Explaining images')):
+        saliency_maps = explainer(img.cuda())
+        explanations[i] = saliency_maps[target[i]].cpu().numpy()
+    return explanations
+
+def apply_mask(image, mask):
+    return image * mask
+
+def random_masking(image, mask_size=50):
+    mask = torch.ones_like(image)
+    x = np.random.randint(0, image.shape[2] - mask_size)
+    y = np.random.randint(0, image.shape[3] - mask_size)
+    mask[:, :, x:x+mask_size, y:y+mask_size] = 0
+    return image * mask
 
 cudnn.benchmark = True
 
@@ -23,7 +70,7 @@ args.workers = 8
 # Directory with images split into class folders.
 # Since we don't use ground truth labels for saliency all images can be 
 # moved to one class folder.
-args.datadir = '/scratch2/Datasets/imagenet/ILSVRC2012_val_folders/'
+args.datadir = '/data/datasets/ImageNet/val/'
 # Sets the range of images to be explained for dataloader.
 args.range = range(95, 105)
 # Size of imput images.
@@ -32,6 +79,7 @@ args.input_size = (224, 224)
 # Use maximum number that the GPU allows.
 args.gpu_batch = 250
 
+## Prepare data
 dataset = datasets.ImageFolder(args.datadir, preprocess)
 
 # This example only works with batch size 1. For larger batches see RISEBatch in explanations.py.
@@ -41,3 +89,56 @@ data_loader = torch.utils.data.DataLoader(
 
 print('Found {: >5} images belonging to {} classes.'.format(len(dataset), len(dataset.classes)))
 print('      {: >5} images will be explained.'.format(len(data_loader) * data_loader.batch_size))
+
+
+## Load model
+model = models.resnet50(True)
+model = nn.Sequential(model, nn.Softmax(dim=1))
+model = model.eval()
+model = model.cuda()
+
+for p in model.parameters():
+    p.requires_grad = False
+    
+# To use multiple GPUs
+model = nn.DataParallel(model)
+
+## create explainer instance
+explainer = RISE(model, args.input_size, args.gpu_batch)
+# Generate masks for RISE or use the saved ones.
+maskspath = 'masks.npy'
+
+if not os.path.isfile(maskspath):
+    explainer.generate_masks(N=6000, s=8, p1=0.1, savepath=maskspath)
+else:
+    explainer.load_masks(maskspath)
+    print('Masks are loaded.')
+
+
+explanations = explain_all(data_loader, explainer)
+explanations.tofile('exp_{:05}-{:05}.npy'.format(args.range[0], args.range[-1]))
+
+for i, (img, _) in enumerate(data_loader):
+    # 설명 가능성 마스크 적용
+    masked_image = apply_mask(img[0], explanations[i])
+    randomly_masked_image = random_masking(masked_image)
+    output = model(randomly_masked_image)
+
+    p, c = torch.max(model(img.cuda()), dim=1)
+    p, c = p[0].item(), c[0].item()
+    
+    plt.figure(figsize=(10, 5))
+    plt.subplot(121)
+    plt.axis('off')
+    plt.title('{:.2f}% {}'.format(100*p, get_class_name(c)))
+    tensor_imshow(img[0])
+    
+    plt.subplot(122)
+    plt.axis('off')
+    plt.title(get_class_name(c))
+    tensor_imshow(img[0])
+    sal = explanations[i]
+    plt.imshow(sal, cmap='jet', alpha=0.5)
+    #plt.colorbar(fraction=0.046, pad=0.04)
+    
+    plt.show()
